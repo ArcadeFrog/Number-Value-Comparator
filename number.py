5765b7eed64bb5d0dc3312dcce4fd9361c6bf4b3
ArcadeFrog <145110198+ArcadeFrog@users.noreply.github.com>
2024-09-28 12:09:43 +0200

    Add files via upload

---
+++ number.py
@@ -0,0 +1,11 @@
+a = 5
+b = 10
+
+if a == b:
+    print("Number A equals Number B")
+
+if a < b:
+    print("Number A is less than Number B")
+
+if a > b:
+    print("Number A is greater than Number B")
