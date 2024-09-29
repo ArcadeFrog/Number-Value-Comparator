6f01deec89819352b93ad40d9178b346c8ce0497
ArcadeFrog <145110198+ArcadeFrog@users.noreply.github.com>
2024-09-29 11:33:56 +0200

    Update number.py
    The program checks if number A or number B is a number or not.

--- number.py
+++ number.py
@@ -1,11 +1,17 @@
 a = 5
 b = 10
 
+if a == NaN:
+    print("Error: Value of number A is invalid.")
+
+if b == NaN:
+    print("Error: Value of number B is invalid.")
+
 if a == b:
     print("Number A equals Number B")
 
 if a < b:
     print("Number A is less than Number B")
-
+    
 if a > b:
     print("Number A is greater than Number B")
